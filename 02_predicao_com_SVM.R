619b0d015f0cb380fc3ced05f3b373470cf4358f
Rafael Lima <ls.rafael@icloud.com>
2019-04-29 21:28:36 -0300

    Preparação dos dados ok

--- 02_predicao_com_SVM.R
+++ 02_predicao_com_SVM.R
@@ -12,5 +12,24 @@ dados$id = NULL
 
 # Criação de um índice randomico para separação dos dados de treino de teste
 dados[ ,'index'] <- ifelse(runif(nrow(dados)) < 0.8,1,0)
-
 View(dados)
+
+# Criação dos Dados de Treino e Teste
+dados_treino <- dados[dados$index == 1, ]
+dados_teste <- dados[dados$index == 0, ]
+
+# Armazenar o índice do dataset de treino
+indice_treino <- grep('index', names(dados_treino))
+
+# Remoção dos índices dos datasets, não serão mais necessários
+dados_treino <- dados_treino[ ,-indice_treino]
+dados_teste <- dados_teste[ ,-indice_treino]
+
+# Obter o índice da coluna da variável target no dataset 
+target_index <- grep('diag',names(dados)) # grep usado para pesquisar a variável que inicia com 'diag'
+
+#### Etapa 2 - Criação do Modelo ####
+# Ajusta-se o kernel para radial, já que o conjunto de dados não tem um 
+# plano linear que pode ser desenhado
+install.packages('e1071')
+library(e1071)
