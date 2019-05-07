a4aede3b2e7579b00d2f421327cb2a4d3fef8600
Rafael Lima <ls.rafael@icloud.com>
2019-05-06 21:57:18 -0300

    commit

--- Exercicios/exercicios_parte_2.R
+++ Exercicios/exercicios_parte_2.R
@@ -1,33 +1,13 @@
 # Lista de Exercícios Parte 2 - Capítulo 11
 
+# Diretório de trabalho
+setwd("/Users/ls_rafael/Documents/GitHub/prevendo_ocorrencia_de_cancer/Exercicios")
+
 # Regressão Linear
 # Definição do Problema: Prever as notas dos alunos com base em diversas métricas
 # https://archive.ics.uci.edu/ml/datasets/Student+Performance
 # Dataset com dados de estudantes
 # Vamos prever a nota final (grade) dos alunos
-
-# Carregando o dataset
-
-# Definir diretório do data set
-setwd('/Users/ls_rafael/Documents/GitHub/prevendo_ocorrencia_de_cancer/Exercicios/student')
-
-# Merge data sets
-d1=read.table("student-mat.csv",sep=";",header=TRUE)
-d2=read.table("student-por.csv",sep=";",header=TRUE)
-
-d3=merge(d1,d2,by=c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet"))
-print(nrow(d3)) # 382 students
-
-df <- read.csv2('estudantes.csv')
-
-# Retornando ao diretório de trabalho
-setwd("/Users/ls_rafael/Documents/GitHub/prevendo_ocorrencia_de_cancer/Exercicios")
-
-# Explorando os dados
-head(df)
-summary(df)
-str(df)
-any(is.na(df))
 
 # install.packages("ggplot2")
 # install.packages("ggthemes")
@@ -35,3 +15,17 @@ any(is.na(df))
 library(ggplot2)
 library(ggthemes)
 library(dplyr)
+
+# Carregando o dataset
+df <- read.csv2('estudantes.csv')
+
+# Explorando os dados
+head(df)
+summary(df)
+str(df)
+any(is.na(df))
+
+
+cor(df$age,df$sex)
+
+
