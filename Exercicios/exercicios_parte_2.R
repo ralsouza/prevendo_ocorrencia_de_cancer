5e1de2b3e9ba0ecf99f9bffe070063309f3a3564
Rafael Lima <ls.rafael@icloud.com>
2019-05-09 00:35:02 -0300

    análise exploratória

--- Exercicios/exercicios_parte_2.R
+++ Exercicios/exercicios_parte_2.R
@@ -29,11 +29,23 @@ any(is.na(df))
 # Checagem da correlação entre as variáveis numéricas
 cor.plot(df[c('age','Medu','Fedu','traveltime','studytime','failures','famrel',
               'freetime','goout','Dalc','Walc','health','absences','G1','G2','G3')],numbers = TRUE)
-# Análise: A correlação entre as variáveis G1, G2 e G3 possuem correlação positiva forte, em torno de 80~85%
+
+# Análise: A correlação entre as variáveis G1, G2 e G3 possuem correlação positiva forte, em torno de 80~90%
 #          Também existe correlação negativa média/moderada entre as variáveis failure e G1, G2 e G3, em torno de 35~36%
 #          Correlação positiva média/moderada entre as variáveis Medu e Fedu, de 62%
 #          Correlação positiva média/moderada entre as variáveis Dalc e Walc, 65%
 #          E uma correlação positiva média/moderada entre as variáveis Walc e goout, 42%
 
-ggplot(df,aes(G3)) + geom_histogram()
+ggplot(df,aes(G1)) + geom_histogram(binwidth = 1)
+ggplot(df,aes(G2)) + geom_histogram(binwidth = 1)
+ggplot(df,aes(G3)) + geom_histogram(binwidth = 1)
+
+# Análise: Houveram quase 40 notas 0 na G3, o que será que houve?
+
+
+ggplot(df,aes(G3, fill = romantic)) + geom_histogram(binwidth = 1)
+
+
+
+
 
