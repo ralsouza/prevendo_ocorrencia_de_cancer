cae24e32e6eeef7772e51c251dd38590dd993d0a
Rafael Lima <ls.rafael@icloud.com>
2019-05-19 23:21:31 -0300

    Predição dos dados de testes

--- Exercicios/exercicios_parte_3.R
+++ Exercicios/exercicios_parte_3.R
@@ -89,6 +89,7 @@ pr_fit_v1 <- predict(lm_fit_v1, df_teste_v1)
 mse_lm_v1 <- sum((pr_fit_v1 - df_teste_v1$medv)^2)/nrow(df_teste_v1) # 15.18%
 
 #### Normalização dos Dados ####
+# Pode ser feito como pre-processamento
 # Redes neurais não são tão fáceis de treinar e ajustar, então alguma preparação é necessária
 # Será usada a técnica de normalização min-max (min-max scale)
 # Normalmente escalando os dados em intervalos [0,1] ou [-1,1] tende a dar resultados melhores
@@ -109,8 +110,15 @@ df_teste_norm_v1 <- as.data.frame(scale(df_teste_v1, center = mins_v1, scale = m
 summary(df_teste_norm_v1)
 
 #### Treinamento do Modelo #### 
+nn_v1 <- neuralnet(medv ~., data = df_treino_norm_v1, hidden = c(5,3), linear.output = TRUE)
 
+plot(nn_v1)
 
+#### Predição MEDV (Valor da Mediana de ocupação das casas) ####
+dim(df_teste_norm_v1)
+# Computar os dados apenas das variáveis preditoras, desconsiderando
+# a variável alvo dos testes
+pred_nn_v1 <- compute(nn, df_teste_norm_v1[ ,1:13])
 
 
 
